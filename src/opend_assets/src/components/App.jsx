67950a07846e5662ed033cb56be261f5039c0b98
NikolSap <nikole397@gmail.com>
2023-10-31 11:42:01 +0200

    openD project

---
+++ src/opend_assets/src/components/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import homeImage from "../../assets/home-img.png";
+import Item from "./Item";
+import "../../assets/main.css"
+import Minter from "./Minter";
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header />
+      {/* <Minter/> */}
+      {/* <Item id={NFTID}/> */}
+      {/* <img className="bottom-space" src={homeImage} /> */}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
